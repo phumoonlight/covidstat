eb64e417e39f0e4e55cdfca0784ad79b223da2c3
Poosarn Moolmuang <phumoonlight@hotmail.com>
2021-03-23 18:02:01 +0700

    format

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
   myCredit = '</> by @phumoonlight'
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

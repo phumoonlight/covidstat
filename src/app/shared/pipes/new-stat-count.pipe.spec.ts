ca27df9250ebeaf8be5a472a84ba53d2f294e73e
Poosarn Moolmuang <phumoonlight@hotmail.com>
2021-03-23 17:21:16 +0700

    format

--- src/app/shared/pipes/new-stat-count.pipe.spec.ts
+++ src/app/shared/pipes/new-stat-count.pipe.spec.ts
@@ -1,8 +1,8 @@
-import { NewStatCountPipe } from './new-stat-count.pipe';
+import { NewStatCountPipe } from './new-stat-count.pipe'
 
 describe('NewStatCountPipe', () => {
   it('create an instance', () => {
-    const pipe = new NewStatCountPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
+    const pipe = new NewStatCountPipe()
+    expect(pipe).toBeTruthy()
+  })
+})
